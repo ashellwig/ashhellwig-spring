496734b13b78dc1baeb6dbdb1d5757d43950d6e6
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-19 03:23:52 -0600

    init

---
+++ ashhellwig-spring/src/main/java/com/ashhellwig/ashhellwigspring/AshhellwigSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ashhellwig.ashhellwigspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshhellwigSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshhellwigSpringApplication.class, args);
+	}
+
+}
